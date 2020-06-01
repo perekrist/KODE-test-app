94934c4b8ea2bcf323b76b832c6929bbaef7c789
perekrist <perekrist31@gmail.com>
2020-06-01 16:48:11 +0700

    Initial Commit

---
+++ KODE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KODE
+//
+//  Created by Кристина Перегудова on 01.06.2020.
+//  Copyright © 2020 Кристина Перегудова. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
